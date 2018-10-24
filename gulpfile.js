1380ad12d7f9b02ea9c84d5500e56e9674652fe3
mtyeyes <mymagicbox@list.ru>
2018-10-24 08:33:52 +0300

    Добавляет черновое оформление index.html и form.html

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,21 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -14,13 +29,32 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(csso())
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task("img", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 4}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/2container/*.svg")
+  .pipe(imagemin([imagemin.svgo()]))
+  .pipe(svgstore({inlineSvg: true}))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,7 +62,29 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("clean", function (){
+  return del("build");
+});
+
+gulp.task("copy", function (){
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/*.*",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+
+gulp.task("start", gulp.series("build", "server"));
---
+++ source/js/feedback.js
@@ -0,0 +1,32 @@
+var sentBtn = document.querySelector('.feedback__btn');
+var failAlert = document.querySelector('.modal-fail');
+var failClose = document.querySelector('.modal-fail__btn');
+var sentAlert = document.querySelector('.modal-sent');
+var sentClose = document.querySelector('.modal-sent__btn');
+var requiredInputs = document.getElementsByClassName("js-required");
+
+sentBtn.addEventListener('click', function( event ) {
+  for (var i = 0 ; i <= requiredInputs.length - 1; i++) {
+    if (requiredInputs[i].checkValidity()) {
+      if (i == requiredInputs.length - 1) {
+        event.preventDefault();
+        sentBtn.disabled = true;
+        sentAlert.classList.remove('modal-sent__closed');
+      }
+    } else {
+      sentBtn.disabled = true;
+      failAlert.classList.remove('modal-fail__closed');
+      break;
+    }
+  }
+});
+
+failClose.addEventListener('click', function() {
+  failAlert.classList.add('modal-fail__closed');
+  sentBtn.disabled = false;
+});
+
+sentClose.addEventListener('click', function() {
+  sentAlert.classList.add('modal-sent__closed');
+  sentBtn.disabled = false;
+});
